537a4918ad842a09034a1fde7652f8ddf9157665
connordshepherd <31867826+connordshepherd@users.noreply.github.com>
2023-05-12 05:22:32 +0000

    All code from Replit

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,18 +1,34 @@
-export default async function (req, res) {
+import axios from 'axios';
 
-  const response = await fetch(process.env.LCC_ENDPOINT_URL, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-Api-Key": process.env.LCC_TOKEN
-    },
-    body: JSON.stringify({
-      question: req.body.question,
-      history: req.body.history
-    }),
-  });
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    return res.status(405).end();
+  }
 
-    const data = await response.json();
+  const { messages } = req.body;
 
-    res.status(200).json({ result: data })
+  try {
+    const response = await axios.post(
+      process.env.BASEPLATE_ENDPOINT,
+        {
+          values: {
+            question: messages[messages.length - 1]?.content || ''
+          },
+          messages: messages.map((message) => ({ role: message.role, content: message.content }))
+        },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${process.env.BASEPLATE_API_KEY}`
+        }
+      }
+    );
+
+    console.log('Baseplate response data:', response.data); // Added this line
+
+    return res.status(200).json(response.data);
+  } catch (error) {
+    console.error('Error querying Baseplate:', error);
+    return res.status(500).json({ error: 'Error querying Baseplate' });
+  }
 }
