e014570b7e7eb624fc583b9d10fe65b763c74d61
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2023-10-21 09:19:09 -0700

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,79 +1,8 @@
-import { createParser } from "eventsource-parser";
-
-export default async function handler(req, res) {
-  console.log("API handler started."); // Debugging statement
-
+export default function handler(req, res) {
   if (req.method !== 'POST') {
-    console.log("Method not POST."); // Debugging statement
-    return res.status(405).end();
+    return res.status(405).end('Method not allowed');
   }
 
-  const { messages } = req.body;
-
-  try {
-    console.log("Sending request to Baseplate."); // Debugging statement
-
-    const fetchResponse = await fetch(process.env.BASEPLATE_ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${process.env.BASEPLATE_API_KEY}`
-      },
-      body: JSON.stringify({
-        messages: messages,
-        stream: true,
-      }),
-    });
-
-    console.log("Received response from Baseplate. Status:", fetchResponse.status); // Debugging statement
-
-    const encoder = new TextEncoder();
-    const decoder = new TextDecoder();
-
-    const stream = new ReadableStream({
-      async start(controller) {
-        function onParse(event) {
-          if (event.type === "event") {
-              const data = event.data;
-              console.log("Data received from Baseplate:", data); // Debugging statement
-      
-              if (data === "[DONE]") {
-                  console.log("Data DONE. Closing stream."); // Debugging statement
-                  controller.close();
-                  return;
-              }
-      
-              try {
-                  const json = JSON.parse(data);
-      
-                  if (!json.choices || json.choices.length === 0) {
-                      console.error("Unexpected data structure:", json);
-                      return;
-                  }
-      
-                  const text = json.choices[0]?.delta?.content || "";
-                  const queue = encoder.encode(text);
-                  controller.enqueue(queue);
-              } catch (e) {
-                  console.error('Error parsing data:', e);
-                  controller.error(e);
-              }
-          }
-      }
-
-
-        const parser = createParser(onParse);
-        for await (const chunk of fetchResponse.body) {
-          parser.feed(decoder.decode(chunk));
-        }
-      },
-    });
-    
-    console.log("Returning response stream."); // Debugging statement
-    return new Response(stream, { headers: { 'Content-Type': 'text/plain' } });
-
-  } catch (error) {
-    console.error('Error querying Baseplate:', error);
-    return res.status(500).json({ error: 'Error querying Baseplate' });
-  }
+  // Just respond with a hardcoded message
+  return res.status(200).send('Hello from the server!');
 }
