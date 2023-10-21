7a389a9c55543cc72892d7d5fdf4178903978922
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2023-10-21 12:30:33 -0700

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,54 +1,38 @@
-import { AIStream } from 'ai';
+export const config = {
+  runtime: 'edge',
+};
 
-// Simple passthrough parser - just returns the chunk as is.
-function passthroughStream() {
-    return data => data;
-}
+const handler = async (req: Request): Promise<Response> => {
+  try {
+    // Extract necessary data from the request
+    const { messages } = (await req.json());
 
-export default async function handler(req, res) {
-    try {
-        // Ensure it's a POST request
-        if (req.method !== 'POST') {
-            console.log("Method not POST");
-            return res.status(405).json({ message: 'Method not POST' });
-        }
+    // Fetch request to Baseplate
+    const fetchResponse = await fetch(process.env.BASEPLATE_ENDPOINT, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${process.env.BASEPLATE_API_KEY}`,
+      },
+      body: JSON.stringify({
+        messages: messages,
+        stream: true,
+      }),
+    });
 
-        // Fetch request to Baseplate
-        const fetchResponse = await fetch(process.env.BASEPLATE_ENDPOINT, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${process.env.BASEPLATE_API_KEY}`
-            },
-            body: JSON.stringify({
-                messages: req.body.messages,
-                stream: true,
-            }),
-        });
-
-        if (!fetchResponse.ok) {
-            console.log(`Received status ${fetchResponse.status} from Baseplate.`);
-            return res.status(fetchResponse.status).json({ message: `Received status ${fetchResponse.status} from Baseplate.` });
-        }
-
-        // Construct the stream
-        const stream = AIStream(fetchResponse, passthroughStream(), {
-            onStart: async () => {
-                console.log('Stream started');
-            },
-            onToken: async token => {
-                console.log('Received chunk:', token);
-            },
-            onFinal: async () => {
-                console.log('Stream completed');
-            },
-        });
-
-        // Directly return the stream wrapped in a Response object
-        return new Response(stream);
-
-    } catch (error) {
-        console.log(`Error in handler: ${error.message}`);
-        return res.status(500).json({ message: `Error in handler: ${error.message}` });
+    if (!fetchResponse.ok) {
+      console.log(`Received status ${fetchResponse.status} from Baseplate.`);
+      return new Response('Error', { status: fetchResponse.status, statusText: `Received status ${fetchResponse.status} from Baseplate.` });
     }
-}
+
+    // Directly use the fetchResponse body as the streaming response
+    const stream = fetchResponse.body;
+
+    return new Response(stream);
+  } catch (error) {
+    console.error(error);
+    return new Response('Error', { status: 500, statusText: error.message });
+  }
+};
+
+export default handler;
