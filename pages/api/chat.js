1fcd6c890bec7e34f98fe96670fc21915a6a7df3
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2023-10-21 09:53:02 -0700

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -3,6 +3,8 @@ import { PassThrough } from 'stream';
 
 export default async function handler(req, res) {
   try {
+    console.log("Function started");
+
     // Ensure it's a POST request
     if (req.method !== 'POST') {
       return res.status(405).json({ message: 'Method not POST' });
@@ -23,21 +25,34 @@ export default async function handler(req, res) {
       }),
     });
 
+    console.log("Baseplate fetch done");
+
     // If fetchResponse is not OK, return the HTTP status we got from Baseplate
     if (!fetchResponse.ok) {
+      console.log("Baseplate fetch not OK");
       return res.status(fetchResponse.status).json({ message: `Received status ${fetchResponse.status} from Baseplate.` });
     }
 
-    // Use a pass-through stream to pipe the Baseplate response directly
+    // Collect a portion of the Baseplate response to log
+    const buffer = await fetchResponse.body.read();
+    if (buffer.value) {
+      console.log("Received from Baseplate:", new TextDecoder().decode(buffer.value));
+    } else {
+      console.log("No data received from Baseplate");
+    }
+
+    // Return early to see if the function gets this far
+    return res.status(200).json({ message: "Got till here!" });
+
+    // The below code is commented out for now
+    /*
     const stream = new PassThrough();
-
-    // Set the response headers
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-
-    // Pipe the Baseplate response through the pass-through stream and to the response
     fetchResponse.body.pipe(stream).pipe(res);
+    */
 
   } catch (error) {
+    console.error("Error in function:", error.message);
     return res.status(500).json({ message: `Error in handler: ${error.message}` });
   }
 }
