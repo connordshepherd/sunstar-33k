1c9342b02b9ab8000f6f82234c57bcb3a5c489d9
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2023-10-21 08:54:47 -0700

    Update chat.js

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -1,13 +1,18 @@
 import { createParser } from "eventsource-parser";
 
 export default async function handler(req, res) {
+  console.log("API handler started."); // Debugging statement
+
   if (req.method !== 'POST') {
+    console.log("Method not POST."); // Debugging statement
     return res.status(405).end();
   }
 
   const { messages } = req.body;
 
   try {
+    console.log("Sending request to Baseplate."); // Debugging statement
+
     const fetchResponse = await fetch(process.env.BASEPLATE_ENDPOINT, {
       method: 'POST',
       headers: {
@@ -20,17 +25,24 @@ export default async function handler(req, res) {
       }),
     });
 
+    console.log("Received response from Baseplate. Status:", fetchResponse.status); // Debugging statement
+
     const encoder = new TextEncoder();
     const decoder = new TextDecoder();
+
     const stream = new ReadableStream({
       async start(controller) {
         function onParse(event) {
           if (event.type === "event") {
             const data = event.data;
+            console.log("Data received from Baseplate:", data); // Debugging statement
+
             if (data === "[DONE]") {
+              console.log("Data DONE. Closing stream."); // Debugging statement
               controller.close();
               return;
             }
+
             try {
               const json = JSON.parse(data);
               const text = json.choices[0]?.delta?.content || "";
@@ -43,7 +55,6 @@ export default async function handler(req, res) {
           }
         }
 
-
         const parser = createParser(onParse);
         for await (const chunk of fetchResponse.body) {
           parser.feed(decoder.decode(chunk));
@@ -51,6 +62,7 @@ export default async function handler(req, res) {
       },
     });
     
+    console.log("Returning response stream."); // Debugging statement
     return new Response(stream, { headers: { 'Content-Type': 'text/plain' } });
 
   } catch (error) {
