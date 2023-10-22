6471476066fa324113795e7c5c705becbbaaa47c
Connor Shepherd <31867826+connordshepherd@users.noreply.github.com>
2023-10-22 15:55:38 -0700

    Update chat.ts

--- pages/api/chat.ts
+++ pages/api/chat.ts
@@ -1,21 +1,23 @@
+import { createParser, ParsedEvent, ReconnectInterval } from "eventsource-parser";
+
 export const config = {
   runtime: 'edge',
 };
 
 const handler = async (req: Request): Promise<Response> => {
   try {
-    // Extract necessary data from the request
-    const { messages } = (await req.json());
+    const body = await req.json();
+    const encoder = new TextEncoder();
+    const decoder = new TextDecoder();
 
-    // Fetch request to Baseplate
-    const fetchResponse = await fetch(process.env.BASEPLATE_ENDPOINT, {
+    const fetchResponse = await fetch(`${process.env.BASEPLATE_ENDPOINT}`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${process.env.BASEPLATE_API_KEY}`,
       },
       body: JSON.stringify({
-        messages: messages,
+        messages: [body.messages],
         stream: true,
       }),
     });
@@ -25,8 +27,32 @@ const handler = async (req: Request): Promise<Response> => {
       return new Response('Error', { status: fetchResponse.status, statusText: `Received status ${fetchResponse.status} from Baseplate.` });
     }
 
-    // Directly use the fetchResponse body as the streaming response
-    const stream = fetchResponse.body;
+    const stream = new ReadableStream({
+      async start(controller) {
+        function onParse(event: ParsedEvent | ReconnectInterval) {
+          if (event.type === "event") {
+            const data = event.data;
+            if (data === "[DONE]") {
+              controller.close();
+              return;
+            }
+            try {
+              const json = JSON.parse(data);
+              const text = json.choices[0].delta?.content || json.choices[0].text || "";
+              const queue = encoder.encode(text);
+              controller.enqueue(queue);
+            } catch (e) {
+              controller.error(e);
+            }
+          }
+        }
+
+        const parser = createParser(onParse);
+        for await (const chunk of fetchResponse.body as any) {
+          parser.feed(decoder.decode(chunk));
+        }
+      },
+    });
 
     return new Response(stream);
   } catch (error) {
